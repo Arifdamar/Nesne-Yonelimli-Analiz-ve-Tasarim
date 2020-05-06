652208b1bf63cacd41575afb03182b1e901c7789
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-05-06 16:18:30 +0300

    Arayüz düzenlendi

--- Arayüz.java
+++ Arayüz.java
@@ -18,6 +18,7 @@ public class Arayüz {
             4 ise -> Cihazın soğutucuyu açıp kapatması esnasındaki durum
          */
         int durum = 1; // Cihaz kapalı olarak başlar
+        boolean sogutucuDurumu = false;
 
         System.out.println("**Kullanıcı Adınızı Giriniz**");
         kullaniciAdi = scan.nextLine();
@@ -33,41 +34,47 @@ public class Arayüz {
                     System.out.println("**Cihazı açmak için 1'e basınız**");
                     secim = scan.nextInt();
                 } while (secim != 1);
+                durum = 3;
+            }
+            IMerkeziIslemBirimiCihaz islemci = new MerkeziIslemBirimiCihaz(); // Cihaz çalıştırılır
+
+            if (durum == 3) {
+                int secim = 0;
+                do {
+                    System.out.println("**Sıcaklığı okumak için 1'e basınız**");
+                    secim = scan.nextInt();
+                } while (secim != 1);
+                islemci.sicaklikOku(); // Sıcaklık okunur
                 durum = 2;
             }
 
 
             if (durum == 2) {
 
-                IMerkeziIslemBirimiCihaz islemci = new MerkeziIslemBirimiCihaz();
-                int secim = 0;
+                int secim;
                 do {
-                    System.out.println("Lütfen Bir İşlem Seçiniz");
-                    System.out.println("1-Sıcaklığı Oku");
-                    System.out.println("2-Soğutucuyu Aç");
-                    System.out.println("3-Soğutucuyu Kapat");
-                    System.out.println("4-Çıkış Yap");
+                    if (!sogutucuDurumu) // Soğutucu kapalıysa açma seçeneğinin arayüzde görüntülenmesi sağlanır
+                        System.out.println("1-Soğutucuyu Aç");
+                    else // Soğutucu açıksa kapatma seçeneğinin arayüzde görüntülenmesi sağlanır
+                        System.out.println("2-Soğutucuyu Kapat");
+
+                    System.out.println("3-Çıkış Yap");
                     secim = scan.nextInt();
 
-                    switch (secim) {
-                        case 1:
-                            islemci.sicaklikOku();
-                            break;
-                        case 2:
-                            islemci.sogutucuAc();
-                            break;
-                        case 3:
-                            islemci.sogutucuKapat();
-                            break;
-                        case 4:
-                            System.out.println("Çıkış Yapılıyor...");
-                            Thread.sleep(1000);
-                            break;
-                        default:
-                            System.out.println("**Lütfen Geçerli Bir Seçenek Giriniz**");
+                    if (secim == 1 && !sogutucuDurumu) { // Soğutucu kapalıysa ve açma seçeneği seçildiyse soğutucu açılır
+                        islemci.sogutucuAc();
+                        sogutucuDurumu = true;
+                    } else if (secim == 2 && sogutucuDurumu) { // Soğutucu açıksa ve kapatma seçeneği seçildiyse soğutucu kapatılır
+                        islemci.sogutucuKapat();
+                        sogutucuDurumu = false;
+                    } else if (secim == 3) {
+                        System.out.println("Çıkış Yapılıyor...");
+                        Thread.sleep(1000);
+                    } else {
+                        System.out.println("**Lütfen Geçerli Bir Seçenek Giriniz**");
                     }
 
-                } while (secim != 4);
+                } while (secim != 3);
             }
 
         }
